a3540bb57d1398845a707d50ac7199a9a1f34350
ToprakArslann <toprakarslan0139@gmail.com>
2025-09-28 18:19:52 +0300

    Done Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import { clash, clashLight, satoshi, satoshiReg } from "./layout";
 import { VscArrowDown } from "react-icons/vsc";
 import { BsArrowUpRight } from "react-icons/bs";
+import { IoMdStar } from "react-icons/io";
 export default function Home() {
   return (
     <div className="w-full min-h-screen flex flex-col items-center justify-start overflow-hidden">
@@ -43,7 +44,7 @@ export default function Home() {
           <div className="flex w-1/3 h-full relative text-4xl text-black py-20 px-5 after:absolute after:h-[94%] after:w-px after:bg-black/40 after:origin-center after:top-1/2 after:-translate-y-1/2 after:right-0">
             <p className={`${satoshiReg.className}`}>Who We Are</p>
           </div>
-          <div className="flex flex-col py-10 px-7 text-5xl w-full relative">
+          <div className="flex flex-col py-10 px-7 text-[2.5vw]/12 w-full relative">
             <span className={`flex h-fit w-full pb-7 relative after:absolute after:w-full after:h-px after:bg-black/40 after:origin-center after:left-1/2 after:-translate-x-1/2 after:bottom-0 ${clash.className}`}>
               We are a multidisciplinary team of designers and <br />
               developers passionate about building products that <br />
@@ -140,6 +141,172 @@ export default function Home() {
         <div className="absolute bottom-0 left-0 px-5 mb-10 flex flex-col w-full leading-65 z-1 text-black/40 font-black">
           <h2 className="uppercase text-[15vw]">DEVORA</h2>
           <h2 className="uppercase text-[15vw] text-end">Systems</h2>
+        </div>
+      </div>
+      <div className={`flex flex-col w-full h-screen relative ${satoshi.className}`}>
+        <Image width={180} height={0} alt="" className="absolute top-10 right-10" src="shape2.svg"/>
+        <Image width={85} height={0} className="absolute top-10 left-10" alt="" src="quotes.svg"/>
+        <div className="flex w-full px-50 py-30 flex-col h-fit">
+          <span className={`flex flex-col gap-5`}>
+            <p className="text-5xl">What our clients say</p>
+            <p className="text-2xl"> -Real stories from the teams we’ve partnered with.</p>
+          </span>
+        </div>
+        <div className="flex flex-row relative w-full h-full mb-20 border-t-1 border-[#666666] items-center justify-stretch">
+          <div className="flex h-full testimonial">
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar2.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">"World-class design and execution. Truly exceeded expectations."</div>
+              <div className="text-2xl text-white w-full flex"> — Michael B., CEO @ Floria</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar1.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">“From concept to launch, the team felt like an extension of our own. Beautiful design, flawless execution.”</div>
+              <div className="text-2xl text-white w-full flex"> — Sofia M., Creative Director @ CineWave</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar3.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">“Working with Devora was like unlocking a cheat code. They transformed our idea into a live product in record time.”</div>
+              <div className="text-2xl text-white w-full flex">— Daniel K., CEO @ Loomio</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar4.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">“They don’t just build products, they craft digital experiences. Truly world-class.”</div>
+              <div className="text-2xl text-white w-full flex">— John R., Product Manager @ NexaPay</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar2.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">"World-class design and execution. Truly exceeded expectations."</div>
+              <div className="text-2xl text-white w-full flex"> — Michael B., CEO @ Floria</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar1.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">“From concept to launch, the team felt like an extension of our own. Beautiful design, flawless execution.”</div>
+              <div className="text-2xl text-white w-full flex"> — Sofia M., Creative Director @ CineWave</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar3.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">“Working with Devora was like unlocking a cheat code. They transformed our idea into a live product in record time.”</div>
+              <div className="text-2xl text-white w-full flex">— Daniel K., CEO @ Loomio</div>
+            </div>
+            <div className="flex flex-col text-black w-120 max-h-[600px] min-h-[500px] items-center px-5 py-5 border-x-1 border-[#666666]/60 border-b-1">
+              <div className="flex flex-row w-full justify-between items-center">
+                <div className=" flex flex-row rounded-full items-center gap-2">
+                  <div className="w-12 aspect-square rounded-full overflow-hidden">
+                    <img alt="" src="avatar4.png" className="object-cover"/>
+                  </div>
+                  <div className="flex flex-row gap-1">
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                    <IoMdStar size={25} className="text-orange-300"/>
+                  </div>
+                </div>
+                <BsArrowUpRight size={25} className="text-white/40"/>
+              </div>
+              <div className="text-4xl text-white py-10 h-full w-full flex">“They don’t just build products, they craft digital experiences. Truly world-class.”</div>
+              <div className="text-2xl text-white w-full flex">— John R., Product Manager @ NexaPay</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
