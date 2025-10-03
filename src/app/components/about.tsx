fec5ae7336dda37bbc97706046c58dc9790b20c1
ToprakArslann <toprakarslan0139@gmail.com>
2025-10-03 20:38:37 +0300

    responsive adjusment into about section

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -15,34 +15,34 @@ export default function About() {
 
     return (
         <section id="about" ref={container} className="w-full h-screen flex flex-col bg-white text-black">
-            <div className="flex flex-row w-full h-full ">
-                <div className="flex w-1/3 h-full relative text-4xl text-black py-20 px-5 after:absolute after:h-[94%] after:w-px after:bg-black/40 after:origin-center after:top-1/2 after:-translate-y-1/2 after:right-0">
+            <div className="flex min-lg:flex-row flex-col w-full h-full ">
+                <div className="flex w-1/3 min-lg:h-full max-lg:w-full relative min-lg:text-4xl text-2xl text-black min-lg:py-20 py-5 px-5 min-lg:after:absolute max-lg:after:hidden after:h-[94%] after:w-px after:bg-black/40 after:origin-center after:top-1/2 after:-translate-y-1/2 after:right-0">
                     <p className={`${satoshiReg.className}`}>Who We Are</p>
                 </div>
-                <div className="flex flex-col py-10 px-7 text-[2.5vw]/12 w-full relative">
-                    <span className={`flex h-fit w-full pb-7 relative after:absolute after:w-full after:h-px after:bg-black/40 after:origin-center after:left-1/2 after:-translate-x-1/2 after:bottom-0 ${clash.className}`}>
+                <div className="flex flex-col py-10 px-7 w-full h-full relative">
+                    <span className={`flex h-fit  w-full min-lg:text-[2.5vw]/12 text-[3.5vw]/8 pb-7 relative after:absolute after:w-full after:h-px after:bg-black/40 after:origin-center after:left-1/2 after:-translate-x-1/2 after:bottom-0 ${clash.className}`}>
                         We are a multidisciplinary team of designers and <br />
                         developers passionate about building products that <br />
                         people love. From strategy to execution, we create <br />
                         seamless digital experiences tailored to your goals.
                     </span>
-                    <div className="flex flex-row w-full items-center justify-between  relative after:absolute after:w-full after:h-px after:bg-black/40 after:origin-center after:left-1/2 after:-translate-x-1/2 after:bottom-0">
-                        <div className="flex h-full w-full py-10 px-10 items-center justify-center text-center leading-8 relative before:absolute before:h-[75%] before:w-px before:bg-black/40 before:top-1/2 before:-translate-y-1/2 before:right-0">
+                    <div className="flex flex-row w-full min-lg:text-[2vw] text-[5vw] items-center justify-between  relative after:absolute after:w-full after:h-px after:bg-black/40 after:origin-center after:left-1/2 after:-translate-x-1/2 after:bottom-0">
+                        <div className="flex h-full w-full min-lg:py-10 py-5 min-lg:px-10 px-5 items-center justify-center text-center leading-8 relative before:absolute before:h-[75%] before:w-px before:bg-black/40 before:top-1/2 before:-translate-y-1/2 before:right-0">
                             <span>
                                 <p className="font-bold">50+</p>
-                                <p className="text-xl">Completed Projects</p>
+                                <p className="min-lg:text-xl text-sm">Completed Projects</p>
                             </span>
                         </div>
-                        <div className="flex h-full w-full items-center justify-center py-10 px-10 text-center leading-8 relative before:absolute before:h-[75%] before:w-px before:bg-black/40 before:top-1/2 before:-translate-y-1/2 before:right-0">
+                        <div className="flex h-full w-full items-center justify-center min-lg:py-10 py-5 min-lg:px-10 px-5 text-center leading-8 relative before:absolute before:h-[75%] before:w-px before:bg-black/40 before:top-1/2 before:-translate-y-1/2 before:right-0">
                             <span>
                                 <p className="font-bold">10+</p>
-                                <p className="text-xl">Team Members</p>
+                                <p className="min-lg:text-xl text-sm">Team Members</p>
                             </span>
                         </div>
-                        <div className="flex h-full w-full items-center justify-center py-10 px-10 text-center leading-8 relative">
+                        <div className="flex h-full w-full items-center justify-center min-lg:py-10 py-5 min-lg:px-10 px-5 text-center leading-8 relative">
                             <span>
                                 <p className="font-bold">12</p>
-                                <p className="text-xl">Clients in Countries</p>
+                                <p className="min-lg:text-xl text-sm">Clients in Countries</p>
                             </span>
                         </div>
                     </div>
